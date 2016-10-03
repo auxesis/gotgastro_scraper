a8c322f830656daaeb9cfd7cca6e360a918f6be3
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-10-03 20:13:37 +1100

    Move logic into a dedicated class

--- scraper.rb
+++ scraper.rb
@@ -13,61 +13,70 @@ require 'date'
 #  'https://api.morph.io/auxesis/nsw_food_authority_prosecution_notices/data.json',
 #]
 
-def url
-  'https://api.morph.io/auxesis/vic_health_register_of_convictions/data.json'
-end
+class Victoria
+  def url
+    'https://api.morph.io/auxesis/vic_health_register_of_convictions/data.json'
+  end
 
-def fetch
-  morph_api_key = ENV['MORPH_API_KEY']
-  params = { :key => morph_api_key, :query => "select * from 'data'" }
-  result = RestClient.get(url, :params => params)
-  @records = JSON.parse(result)
-end
+  def morph_api_key
+    ENV['MORPH_API_KEY']
+  end
 
-def geocoded
-  @records.select { |r|
-    !r['lat'].blank? && !r['lng'].blank?
-  }
-end
+  def fetch
+    params = { :key => morph_api_key, :query => "select * from 'data'" }
+    result = RestClient.get(url, :params => params)
+    @records = JSON.parse(result)
+  end
 
-def md5(string)
-  @hash ||= Digest::MD5.new
-  @hash.hexdigest(string)
-end
-
-def businesses
-  return @businesses if @businesses
-
-  @businesses = geocoded.map do |r|
-    {
-      'id'      => md5(r['address']),
-      'name'    => r['trading_name'],
-      'lat'     => r['lat'].to_f,
-      'lng'     => r['lng'].to_f,
-      'address' => r['address'],
+  def geocoded
+    @records.select { |r|
+      !r['lat'].blank? && !r['lng'].blank?
     }
   end
 
-  @businesses.uniq! {|b| b['id'] }
-end
+  def md5(string)
+    @hash ||= Digest::MD5.new
+    @hash.hexdigest(string)
+  end
 
-def offences
-  return @offences if @offences
+  def businesses
+    return @businesses if @businesses
 
-  @offences = geocoded.map do |r|
-    {
-      'business_id' => md5(r['address']),
-      'date'        => Date.parse(r['conviction_date']),
-      'link'        => r['link'],
-      'description' => r['description'],
-    }
+    @businesses = geocoded.map do |r|
+      {
+        'id'      => md5(r['address']),
+        'name'    => r['trading_name'],
+        'lat'     => r['lat'].to_f,
+        'lng'     => r['lng'].to_f,
+        'address' => r['address'],
+      }
+    end
+
+    @businesses.uniq! {|b| b['id'] }
+  end
+
+  def offences
+    return @offences if @offences
+
+    @offences = geocoded.map do |r|
+      {
+        'business_id' => md5(r['address']),
+        'date'        => Date.parse(r['conviction_date']),
+        'link'        => r['link'],
+        'description' => r['description'],
+      }
+    end
   end
 end
 
-def save
-  ScraperWiki.save_sqlite(['id'], businesses, 'businesses')
-  ScraperWiki.save_sqlite(['link'], offences, 'offences')
+class NSW
 end
 
-fetch
-save
+sources = [ Victoria.new ]
+sources.each {|s| s.fetch }
+
+businesses = sources.map {|s| s.businesses }.flatten
+offences   = sources.map {|s| s.offences   }.flatten
+
+ScraperWiki.save_sqlite(['id'], businesses, 'businesses')
+ScraperWiki.save_sqlite(['link'], offences, 'offences')
